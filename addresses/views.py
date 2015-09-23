90b069e8e657035c8302c584c75f736722245c26
Jos'e Guilherme Tavares <zetavares.rib@gmail.com>
2015-09-22 23:17:28 -0300

    Update views.py
    Luan
    Seguem as sugestões
    Se tiver alguma dúvida ou quiser trocar uma idéia, me chama no slack do grupo de python de RP amanhã
    Abraços

--- addresses/views.py
+++ addresses/views.py
@@ -29,6 +29,9 @@ class AddressListView(FieldsMixin, ListEndpoint):
     def get_query_set(self, request, *args, **kwargs):
         qs = super(AddressListView, self).get_query_set(request, *args,
                                                         **kwargs)
+        # Complementar o limit com paginação
+        # request.params.get('page')
+        
         # TODO implement a start
         limit = request.params.get('limit')
         if limit:
@@ -41,17 +44,71 @@ class AddressListView(FieldsMixin, ListEndpoint):
         return qs
 
     def post(self, request, *args, **kwargs):
-        zipcode = request.data.get('zipcode')
-        if not zipcode:
-            zipcode = request.data.get('zip_code')
+        zipcode = request.data.get('zipcode') or request.data.get('zip_code')
+        
+        # if not zipcode:
+            # zipcode = request.data.get('zip_code')
 
         if not zipcode:
             return Http400(_('zipcode not sent'))
-
+        
+        # Usaria um Client aqui, em outro .py rs
+        # Só uma idéia, por exemplo:
+        # Separar os métodos ajudaria muito para testar, inserir o novo registro de forma assincrona e para alterar o postmon
+        # Considerando o ZipCodeModelForm um ModelForm de Address
+        # client.py
+        # class ZipCodeClient(object):
+        #     def __init__(self, zipcode):
+        #         self.zipcode = zipcode
+            
+        #     def get_form_data(self):
+        #         # aqui que facilitaria numa eventual troca de serviço
+        #         response_data = self.get_response()
+        #         return {
+        #             'zipcode': response_data.get('cep'),
+        #             'address': response_data.get('logradouro'),
+        #             'neighborhood': response_data.get('bairro'),
+        #             'state': response_data.get('estado'),
+        #             'city': response_data.get('cidade'),
+        #             'address2': response_data.get('complemento'),
+        #         }
+        #     def get_response(self):
+        #         response = requests.get(
+        #             '{}{}'.format(
+        #                 settings.ZIPCODE_API_URL,
+        #                 self.zipcode
+        #             )
+        #         )
+        #         return response.json()
+            
+        #     def get_zipcode(self):
+        #         try:
+        #             return Address.objects.get(zipcode=zipcode)
+        #         except:
+        #             pass
+                
+        #         data = self.get_form_data()
+        #         form = ZipCodeModelForm(data)
+        #         if form.is_valid():
+        #             return form.save()
+        #         return None # ou um raise
+        
+        # Usando o client
+        # client = ZipCodeClient(zipcode)
+        # zipcode = client.get_zipcode()
+        # if zipcode:
+        #     return Http201(self.serialize(obj))
+        # return Http400(_('zipcode invalid'))
+        
+        # - - - - - - - - - - -
+        
         form = ZipCodeForm({'zipcode': zipcode})
+        # validar formato e se existe não acho muito interessante
+        # poderia fazer um ljust e a api de consulta trata os ceps invalidos: ex. 12345 formata para 00012345 por exemplo
         if not form.is_valid():
             return Http400(_('invalid data'), errors=form.errors)
-
+        
+        # timeout no get
         response = requests.get(
             '{}{}'.format(
                 settings.ZIPCODE_API_URL,
@@ -62,11 +119,12 @@ class AddressListView(FieldsMixin, ListEndpoint):
             return Http404(_(u'zipcode not found'))
 
         response = response.json()
-
+        
         obj = Address.objects.create(
             zipcode=response['cep'],
             address=response.get('logradouro'),  # nullable
             neighborhood=response.get('bairro'),  # nullable
+            # forçar/esperar um erro KeyError aqui acho que não fica legal, até por isso o client
             state=response['estado'],
             city=response['cidade'],
             address2=response.get('complemento'),  # nullable
